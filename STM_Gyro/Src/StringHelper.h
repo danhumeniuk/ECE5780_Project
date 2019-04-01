0ee6bbc2c868e673bf15ab672a84c3a2d1ea79e9
danhumeniuk <35534588+danhumeniuk@users.noreply.github.com>
2019-04-01 17:57:12 -0600

    Cited sources

--- STM_Gyro/Src/StringHelper.h
+++ STM_Gyro/Src/StringHelper.h
@@ -1,3 +1,9 @@
+/* This function converts an integer value to a char pointer
+ * Made possible with help from : 
+ *  https://stackoverflow.com/questions/8257714/how-to-convert-an-int-to-string-in-c*/
+ * https://github.com/tmdarwen/STM32/blob/master/STM32F411/OnboardGyroAccel/ 
+ */
+
 #define BASE_10  10
 
 char* IntegerToString(int value, char *result, int base);
