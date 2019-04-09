7a7d9e75c5b9349e3d100b7acd3ead4139c5ad9a
danhumeniuk <35534588+danhumeniuk@users.noreply.github.com>
2019-04-09 10:16:30 -0600

    Updated USART.h

--- STM_Gyro/Inc/USART.h
+++ STM_Gyro/Inc/USART.h
@@ -1,6 +1,11 @@
+#include "main.h"
 
-void usart_init(void);
+void USART_INIT(uint16_t RX, uint16_t TX, GPIO_TypeDef * RX_GPIO, GPIO_TypeDef * TX_GPIO, int baud);
 
-void transmit_str(char c[]);
+void TRANSMIT_STR(char c[]);
 
-void transmit_char(char c);
+void TRANSMIT_CHAR(char c);
+
+void USART1_Enable(void);
+
+void SET_ALT_FUNC_USART1(void);
