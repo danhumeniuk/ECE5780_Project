2e33bd207ab09a3a0e2ffd6029cba961df18790d
danhumeniuk <35534588+danhumeniuk@users.noreply.github.com>
2019-04-18 18:27:12 -0600

    Removed HAL library and switched to bit operations

--- STM_Gyro/Src/LED.c
+++ STM_Gyro/Src/LED.c
@@ -2,12 +2,14 @@
 
 void init_LEDs(void)
 {	
-	GPIO_InitTypeDef initLED = 
-	{
-		GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9,
-		GPIO_MODE_OUTPUT_PP,
-		GPIO_SPEED_FREQ_LOW,
-		GPIO_NOPULL
-	};
-	HAL_GPIO_Init(GPIOC, &initLED);
+	/* Initialize Blue and Red LEDs */
+	GPIOC->MODER |=	(1 << 14) | (1 << 12); //SET bits 14 and 12 bit for Pins 6-7
+	
+	GPIOC->MODER &=	~((1 << 15) | (1 << 13)); //Clear bits 15 and 13 for Pins 6-7
+														
+	GPIOC->OTYPER &=	~((1 << 7) | (1 << 6)); //Clears bits 7 and 6 for Pins 6-7
+														
+	GPIOC->OSPEEDR &= ~((1 << 14) | (1 << 12)); //SET bits 14 and 12 bit for Pins 6-7
+														
+	GPIOC->PUPDR &=	~((1 << 15) | (1 << 14 ) | (1 << 13) | (1 << 12)); //Clears bits 15 - 12
 }
