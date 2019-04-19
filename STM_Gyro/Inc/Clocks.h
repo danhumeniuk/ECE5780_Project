c606b31b1e7c3aecb13c26977dc17ffbfdc0f835
danhumeniuk <35534588+danhumeniuk@users.noreply.github.com>
2019-04-18 18:40:43 -0600

    Added Clocks library which enables the clocks

---
+++ STM_Gyro/Inc/Clocks.h
@@ -0,0 +1,14 @@
+/*
+ * These functions are to enable various clocks
+ * on the STM32
+ */
+
+void init_gpioa_clk(void);
+
+void init_gpiob_clk(void);
+
+void init_gpioc_clk(void);
+
+void init_usart1_clk(void);
+
+void init_i2c2_clk(void);
---
+++ STM_Gyro/Src/Clocks.c
@@ -0,0 +1,47 @@
+#include "Clocks.h"
+#include "main.h"
+
+/*
+ * Helper function that has the sole purpose of
+ * enabling the GPIOA Clock
+ */
+void init_gpioa_clk(void)
+{
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;	
+}
+
+/*
+ * Helper function that has the sole purpose of
+ * enabling the GPIOB Clock
+ */
+void init_gpiob_clk(void)
+{
+	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+}
+
+/*
+ * Helper function that has the sole purpose of
+ * enabling the GPIOC Clock
+ */
+void init_gpioc_clk(void)
+{
+	RCC->AHBENR |= RCC_AHBENR_GPIOCEN;	
+}
+
+/*
+ * Helper function that has the sole purpose of
+ * enabling the USART1 Clock
+ */
+void init_usart1_clk(void)
+{
+	RCC->APB2ENR |= RCC_APB2ENR_USART1EN;	
+}
+
+/*
+ * Helper function that has the sole purpose of
+ * enabling the I2C2 Clock
+ */
+void init_i2c2_clk(void)
+{
+	RCC->APB1ENR |= RCC_APB1ENR_I2C2EN;
+}
